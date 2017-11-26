5bed096aebb20b955eba3f5dbfe850eff7a07f9e
Vinicius Schulte <schultev2@gmail.com>
2017-11-26 02:29:45 -0200

    Adicionado arquivo .pl

---
+++ carros.pl
@@ -0,0 +1,466 @@
+fabricado(rolls-royce, reino-unido).
+fabricado(peugeot, frança).
+fabricado(maserati, italia).
+fabricado(ferrari, italia).
+fabricado(bmw, alemanha).
+fabricado(lamborghini, italia).
+fabricado(porsche, alemanha).
+fabricado(bugatti, frança).
+fabricado(audi, alemanha).
+fabricado(toyota, japao).
+fabricado(volkswagen, alemanha). 
+fabricado(ford, estados-unidos).
+fabricado(nissan, japao).
+fabricado(hyundai, coreia-do-sul).
+fabricado(honda, japao).
+fabricado(chevrolet, estados-unidos).
+fabricado(kia, coreia-do-sul).
+fabricado(renault, franca).
+fabricado(merecedez-benz, alemanha).
+fabricado(mazda, japao).
+fabricado(fiat, italia).
+fabricado(jeep, estados-unidos).
+fabricado(chrysler, estados-unidos).
+fabricado(suruba, estados-unidos).
+fabricado(tesla, estados-unidos).
+
+modelo(phantom, rolls-royce).
+modelo(wraith, rolls-royce).
+modelo(dawn, rolls-royce).
+modelo(ghost, rolls-royce).
+modelo(cullinan, rolls-royce).
+modelo(sweptail, rolls-royce).
+
+modelo(208, pegeout).
+modelo(308, pegeout).
+modelo(408, pegeout).
+modelo(2008, pegeout).
+modelo(partner, pegeout).
+
+modelo(ghibli, maserati).
+modelo(levante, maserati).
+modelo(quattroporte, maserati).
+modelo(granTurismo, maserati).
+modelo(granCabrio, maserati).
+
+modelo(488-spider, ferrari).
+modelo(488-gTB, ferrari).
+modelo(812-superFast, ferrari).
+modelo(portofino, ferrari).
+modelo(gtc4Lusso-t, ferrari).
+modelo(gtc4Lusso, ferrari).
+modelo(f12tdf, ferrari).
+modelo(laFerrari, ferrari).
+modelo(laFerrari-aperta, ferrari).
+
+modelo(m140i, bmw).
+modelo(m240i, bmw).
+modelo(x1, bmw).
+modelo(x3, bmw).
+modelo(x4, bmw).
+modelo(x5, bmw).
+modelo(x6, bmw).
+modelo(m2, bmw).
+modelo(m3, bmw).
+modelo(m6, bmw).
+modelo(x6-m, bmw).
+modelo(i3, bmw).
+modelo(i8, bmw).
+
+modelo(aventador, lamborghini).
+modelo(huracan-spider, lamborghini).
+modelo(centenario, lamborghini).
+modelo(veneno, lamborghini).
+modelo(urus, lamborghini).
+
+modelo(718-cayman, porsche).
+modelo(718-boxter, porsche).
+modelo(911-carrera, porsche).
+modelo(911-targa4, porsche).
+modelo(911-turboS, porsche).
+modelo(panamera-turbo, porsche).
+modelo(macan, porsche).
+modelo(cayenne, porsche).
+
+modelo(veyron, bugatti).
+modelo(cheyron, bugatti).
+
+modelo(r8, audi).
+modelo(a1, audi).
+modelo(a3, audi).
+modelo(a4, audi).
+modelo(a6, audi).
+modelo(a7, audi).
+modelo(q3, audi).
+modelo(q7, audi).
+modelo(tt, audi).
+
+modelo(corolla, toyota).
+modelo(hilux, toyota).
+modelo(etios, toyota).
+modelo(sw4, toyota).
+modelo(rav4, toyota).
+modelo(prius, toyota).
+
+modelo(fusca, volkswagen).
+modelo(polo, volkswagen).
+modelo(gol, volkswagen).
+modelo(up, volkswagen).
+modelo(fox, volkswagen).
+modelo(spacefox, volkswagen).
+modelo(golf, volkswagen).
+modelo(voyage, volkswagen).
+modelo(jetta, volkswagen).
+modelo(passat, volkswagen).
+modelo(golf-variant, volkswagen).
+modelo(tiguan, volkswagen).
+modelo(touareg, volkswagen).
+modelo(saveiro, volkswagen).
+modelo(amarok, volkswagen).
+
+modelo(ka, ford).
+modelo(fiest, ford).
+modelo(focus, ford).
+modelo(fusion, ford).
+modelo(mustang, ford).
+modelo(ranger, ford).
+modelo(ecosport, ford).
+modelo(edge, ford).
+
+modelo(versa, nissan).
+modelo(march, nissan).
+modelo(sentra, nissan).
+modelo(kicks, nissan).
+modelo(frontier, nissan).
+modelo(gt-r, nissan).
+
+modelo(creta, hyundai).
+modelo(hb20, nissan).
+modelo(i30, nissan).
+modelo(elantra, nissan).
+modelo(azera, nissan).
+modelo(tucson, nissan).
+modelo(ix35, nissan).
+modelo(santa-fe, nissan).
+
+modelo(civic, honda).
+modelo(city, honda).
+modelo(fit, honda).
+modelo(wr-v, honda).
+modelo(hr-v, honda).
+modelo(cr-v, honda).
+modelo(accord, honda).
+
+modelo(onix, chevrolet).
+modelo(prisma, chevrolet).
+modelo(cobalt, chevrolet).
+modelo(spin, chevrolet).
+modelo(cruze, chevrolet).
+modelo(tracker, chevrolet).
+modelo(equinox, chevrolet).
+modelo(trailblazer, chevrolet).
+modelo(montana, chevrolet).
+modelo(s10, chevrolet).
+modelo(camaro, chevrolet).
+modelo(corvette, chevrolet).
+
+modelo(soul, kia).
+modelo(picanto, kia).
+modelo(cerato, kia).
+modelo(optima, kia).
+modelo(cadenza, kia).
+modelo(quoris, kia).
+modelo(sportage, kia).
+modelo(sorento, kia).
+modelo(mohave, kia).
+
+modelo(sandero, renault).
+modelo(logan, renault).
+modelo(kwid, renault).
+modelo(captur, renault).
+modelo(duster, renault).
+modelo(duster-orochi, renault).
+modelo(fluence, renault).
+modelo(kangoo, renault).
+
+modelo(class-a, merecedez-benz).
+modelo(class-b, merecedez-benz).
+modelo(class-c, merecedez-benz).
+modelo(cla, merecedez-benz).
+modelo(cls, merecedez-benz).
+modelo(class-e, merecedez-benz).
+modelo(class-g, merecedez-benz).
+modelo(gla, merecedez-benz).
+modelo(glc, merecedez-benz).
+modelo(gle, merecedez-benz).
+modelo(gls, merecedez-benz).
+modelo(class-s, merecedez-benz).
+modelo(sl, merecedez-benz).
+modelo(slc, merecedez-benz).
+modelo(amg, merecedez-benz).
+modelo(amg-gt, merecedez-benz).
+
+modelo(cx3, mazda).
+modelo(mazda2, mazda).
+modelo(mazda3, mazda).
+modelo(mazda6, mazda).
+modelo(cx5, mazda).
+modelo(mx5, mazda).
+
+modelo(toro, fiat).
+modelo(mobi, fiat).
+modelo(uno, fiat).
+modelo(argo, fiat).
+modelo(palio, fiat).
+modelo(punto, fiat).
+modelo(500, fiat).
+modelo(grand-siena, fiat).
+modelo(weekend, fiat).
+modelo(strada, fiat).
+modelo(doblo, fiat).
+modelo(ducato, fiat).
+
+modelo(cherokee, jeep).
+modelo(gran-cherokee, jeep).
+modelo(renegade, jeep).
+modelo(compass, jeep).
+modelo(wrangler, jeep).
+
+modelo(c300c ,chrysler).
+modelo(town-e-country ,chrysler).
+
+modelo(impreza ,suruba).
+modelo(xv ,suruba).
+modelo(forester ,suruba).
+modelo(legacy ,suruba).
+modelo(outback ,suruba).
+modelo(wrx ,suruba).
+modelo(wrx-sti ,suruba).
+
+modelo(s, tesla).
+modelo(x, tesla).
+modelo(3, tesla).
+modelo(roadster-2009, tesla).
+modelo(roadster, tesla).
+
+tipo(coupe, phantom).
+tipo(coupe, wraith).
+tipo(coupe, dawn).
+tipo(coupe, ghost).
+tipo(suv, cullinan).
+tipo(sedan, sweptail).
+
+tipo(hatchback, 208).
+tipo(hatchback, 308).
+tipo(sedan, 408).
+tipo(suv, 2008).
+tipo(van, partner).
+
+tipo(sedan, ghibli).
+tipo(suv, levante).
+tipo(sedan, quattroporte).
+tipo(coupe, granTurismo).
+tipo(coupe, granCabrio).
+
+tipo(esportivo, 488-spider).
+tipo(esportivo, 488-gTB).
+tipo(esportivo, 812-superFast).
+tipo(coupe, portofino).
+tipo(coupe, gtc4Lusso-t).
+tipo(coupe, gtc4Lusso).
+tipo(coupe, f12tdf).
+tipo(esportivo, laFerrari).
+tipo(esportivo, laFerrari-aperta).
+
+tipo(hatchback, m140i).
+tipo(coupe, m240i).
+tipo(suv, x1).
+tipo(suv, x3).
+tipo(suv, x4).
+tipo(suv, x5).
+tipo(suv, x6).
+tipo(coupe, m2).
+tipo(coupe, m3).
+tipo(coupe, m6).
+tipo(suv, x6-m).
+tipo(hatchback, i3).
+tipo(esportivo, i8).
+
+tipo(esportivo, aventador).
+tipo(esportio, huracan-spider).
+tipo(esportivo, centenario).
+tipo(esportivo, veneno).
+tipo(suv, urus).
+
+tipo(coupe, 718-cayman).
+tipo(coupe, 718-boxter).
+tipo(coupe, 911-carrera ).
+tipo(coupe, 911-targa4).
+tipo(coupe, turboS).
+tipo(sedan, panarema_turbo).
+tipo(suv, macan).
+tipo(suv, cayenne).
+
+tipo(esportivo, veyron).
+tipo(esportivo, cheyron).
+
+tipo(esportivo, r8).
+tipo(hatchback, a1).
+tipo(coupe, a3).
+tipo(sedan, a4).
+tipo(sedan, a6).
+tipo(sedan, a7).
+tipo(suv, q3).
+tipo(suv, q7).
+tipo(copue, tt).
+
+tipo(sedan, corolla).
+tipo(pickup, hilux).
+tipo(hatch, etios).
+tipo(pickup, sw4).
+tipo(pickup, rav4).
+tipo(sedan, prius).
+
+tipo(coupe, fusca).
+tipo(hatchback, polo).
+tipo(hatchback, gol).
+tipo(hatchback, up).
+tipo(hatchback, fox).
+tipo(hatchback, spacefox).
+tipo(hatchback, golf).
+tipo(sedan, voyage).
+tipo(sedan, jetta).
+tipo(sedan, passat).
+tipo(perua, golf-variant).
+tipo(suv, tiguan).
+tipo(suv, touareg).
+tipo(pickup, saveiro).
+tipo(pickup, amarok).
+
+tipo(hatchback, ka).
+tipo(sedan, ka).
+tipo(hatchback, fiesta).
+tipo(sedan, fiesta).
+tipo(hatchback, focus).
+tipo(sedan, focus).
+tipo(sedan, fusion).
+tipo(muscle, mustang).
+tipo(pickup, ranger).
+tipo(suv, ecosport).
+tipo(suv, edge).
+
+tipo(sedan, versa).
+tipo(hatchback, march).
+tipo(sedan, sentra).
+tipo(hatchback, kicks).
+tipo(pickup, frontier).
+tipo(coupe, gt-r).
+
+tipo(suv, creta).
+tipo(hatchback, hb20).
+tipo(sedan, hb20).
+tipo(hatchback, i30).
+tipo(sedan, elantra).
+tipo(sedan, azera).
+tipo(suv, tucson).
+tipo(suv, ix35).
+tipo(suv, santa-fe).
+
+tipo(sedan, civic).
+tipo(sedan, city).
+tipo(hatchback, fit).
+tipo(suv, wr-v).
+tipo(suv, hr-v).
+tipo(suv, cr-v).
+tipo(sedan, accord).
+
+tipo(hatchback, onix).
+tipo(sedan, prisma).
+tipo(sedan, cobalt).
+tipo(van, spin).
+tipo(hatchback, cruze).
+tipo(suv, tracker).
+tipo(suv, equinox).
+tipo(suv, trailblazer).
+tipo(pickup, montana).
+tipo(pickup, s10).
+tipo(muscle, camaro).
+tipo(coupe, corvette).
+
+tipo(hatchback, soul).
+tipo(hatchback, picanto).
+tipo(sedan, cerato).
+tipo(sedan, optima).
+tipo(sedan, cadenza).
+tipo(sedan, quoris).
+tipo(suv, sportage).
+tipo(suv, sorento).
+tipo(suv, mohave).
+
+tipo(hatchback, sandero).
+tipo(sedan, logan).
+tipo(hatchback, kwid).
+tipo(suv, captur).
+tipo(suv, duster).
+tipo(suv, duster-orochi).
+tipo(sedan, fluence).
+tipo(van, kangoo).
+
+tipo(hatchback, class-a).
+tipo(perua, class-b).
+tipo(sedan, class-c).
+tipo(coupe, cla).
+tipo(sedan, cls).
+tipo(sedan, class-e).
+tipo(jipe, class-g).
+tipo(suv, gla).
+tipo(suv, glc).
+tipo(suv, gle).
+tipo(suv, gls).
+tipo(sedan, class-s).
+tipo(coupe, sl).
+tipo(coupe, slc).
+tipo(coupe, amg).
+tipo(coupe, amg-gt).
+
+tipo(hatchback, cx3).
+tipo(hatchback, mazda2).
+tipo(sedan, mazda3).
+tipo(suv, mazda6).
+tipo(suv, cx5).
+tipo(coupe, mx5).
+
+tipo(pickup, toro).
+tipo(hatchback, mobi).
+tipo(hatchback, uno).
+tipo(hatchback, argo).
+tipo(hatchback, palio).
+tipo(hatchback, punto).
+tipo(hatchback, 500).
+tipo(sedan, grand-siena).
+tipo(perua, weekend).
+tipo(van, doblo).
+tipo(van, ducato).
+
+tipo(jipe, cherokee).
+tipo(suv, gran-cherokee).
+tipo(suv, renegade).
+tipo(suv, compass).
+tipo(jipe, wrangler).
+
+tipo(sedan, 300c).
+tipo(van, town-e-country).
+
+tipo(sedan, impreza).
+tipo(suv, xv).
+tipo(suv, forester).
+tipo(sedan, legacy).
+tipo(suv, outback).
+tipo(coupe, wrx).
+tipo(coupe, wrx-sti).
+
+tipo(sedan, s).
+tipo(suv, x).
+tipo(coupe, 3).
+tipo(coupe, roadster-2009).
+tipo(coupe, roadster).
